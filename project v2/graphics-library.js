549f57c043b3b7c18ecd55ce1e01dee3610f742d
Yurqles <57115688+Yurqles@users.noreply.github.com>
2020-10-16 15:24:09 -0600

    Update graphics-library.js

--- project v2/graphics-library.js
+++ project v2/graphics-library.js
@@ -74,62 +74,106 @@ function mousemoveHandlerLib(event) {
 
 //Create an Array to represent the where the platforms will be 
 let platforms = [];
+let platforms2 = [];
 platforms.push({
-        x: 250,
-        y: 430,
-        w: 100,
-        h: 16,
-    },{
-        x: 460,
-        y: 400,
-        w: 100,
-        h: 16,
-    },{
-        x: -40,
-        y: 370,
-        w: 100,
-        h: 16,
-    },{
-        x: 140,
-        y: 340,
-        w: 100,
-        h: 16,
-    },{
-        x: 340,
-        y: 310,
-        w: 100,
-        h: 16,
-    },{
-        x: 520,
-        y: 280,
-        w: 100,
-        h: 16,
-    },{
-        x: -40,
-        y: 250,
-        w: 100,
-        h: 16,
-    },{
-        x: 140,
-        y: 220,
-        w: 100,
-        h: 16,
-    },{
-        x: 340,
-        y: 190,
-        w: 100,
-        h: 16,
-    },{
-        x: 520,
-        y: 160,
-        w: 100,
-        h: 16,
-    },)
+    x: 250,
+    y: 430,
+    w: 100,
+    h: 16,
+},{
+    x: 460,
+    y: 400,
+    w: 100,
+    h: 16,
+},{
+    x: -40,
+    y: 370,
+    w: 100,
+    h: 16,
+},{
+    x: 140,
+    y: 340,
+    w: 100,
+    h: 16,
+},{
+    x: 340,
+    y: 310,
+    w: 100,
+    h: 16,
+},{
+    x: 520,
+    y: 280,
+    w: 100,
+    h: 16,
+},{
+    x: -40,
+    y: 250,
+    w: 100,
+    h: 16,
+},{
+    x: 140,
+    y: 220,
+    w: 100,
+    h: 16,
+},{
+    x: 340,
+    y: 190,
+    w: 100,
+    h: 16,
+},{
+    x: 520,
+    y: 160,
+    w: 100,
+    h: 16,
+},)
+//For second level
+platforms2.push({
+    x: 520,
+    y: 62,
+    w: 100,
+    h: 16,
+},{
+    x: 300,
+    y: 170,
+    w: 100,
+    h: 16,
+},{
+    x: 100,
+    y: 290,
+    w: 100,
+    h: 16,
+},{
+    x: 500,
+    y: 290,
+    w: 100,
+    h: 16,
+},{
+    x: 300,
+    y: 390,
+    w: 100,
+    h: 16,
+},{
+    x: 100,
+    y: 490,
+    w: 100,
+    h: 16,
+}, {
+    x: 500,
+    y: 490,
+    w: 100,
+    h: 16,
+})
 
+//Stage number
+let stage = 1;
+
+
+//Background
+let background = [document.getElementById('background'), document.getElementById('background2')]
     
 let player = {
     x: 0,
-    y: 500,
+    y: 0,
     w: 32,
     h: 32,
     x_velocity: 0, // move x
@@ -140,45 +184,124 @@ let player = {
     jumping: false,
 }
 
+//Bat properties
+let bats = []; //16 x 18
+bats.push({
+    x: 0,
+    y: 85,
+    w: 32,
+    h: 32,
+    spriteX: 0,
+    spriteY: 0,
+},{
+    x: 580,
+    y: 200,
+    w: 32,
+    h: 32,
+    spriteX: 0,
+    spriteY: 0,
+},{
+    x: 0,
+    y: 200,
+    w: 32,
+    h: 32,
+    spriteX: 0,
+    spriteY: 0,
+},{
+    x: 580,
+    y: 315,
+    w: 32,
+    h: 32,
+    spriteX: 0,
+    spriteY: 0,
+},{
+    x: 0,
+    y: 315,
+    w: 32,
+    h: 32,
+    spriteX: 0,
+    spriteY: 0,
+},{
+    x: 580,
+    y: 410,
+    w: 32,
+    h: 32,
+    spriteX: 0,
+    spriteY: 0,
+},{
+    x: 0,
+    y: 410,
+    w: 32,
+    h: 32,
+    spriteX: 0,
+    spriteY: 0,
+},)
+
 //Spritesheet 16 x 16
 let spriteSheetImg = document.getElementById('sprite-sheet');
 let spriteY = 0;
 let spriteX = 66;
-//Background
-let background = document.getElementById('background');
+
 //Platforms with the width and height
 let platform = new Image();
-platform.src = 'images/platform.png';
+platform.src = 'images/platform.png'; 
+let bat = new Image();
+bat.src = 'images/bats.png';
 
-function drawPlatforms(n) {
-    ctx.drawImage(platform, platforms[n].x, platforms[n].y, platforms[n].w, platforms[n].h)
-}   
-
-function horizontalCollision() {
-    for (i = 0; i < platforms.length; i++) {
-        if (RectCollision(player, platforms, i)) { 
-            if (player.x_velocity > 0) {//left side
-                player.x = platforms[i].x - player.w; 
-                player.x_velocity = 0;
-            } else if (player.x_velocity < 0) {//right side
-                player.x = platforms[i].x + platforms[i].w; 
-                player.x_velocity = 0;
-            } 
+function horizontalCollision() { 
+    if (stage == 1) {
+        for (i = 0; i < platforms.length; i++) {
+            if (RectCollision(player, platforms, i)) { 
+                if (player.x_velocity > 0) {//left side
+                    player.x = platforms[i].x - player.w; 
+                    player.x_velocity = 0;
+                } else if (player.x_velocity < 0) {//right side
+                    player.x = platforms[i].x + platforms[i].w; 
+                    player.x_velocity = 0;
+                } 
+            }
+        }
+    } else if (stage == 2) {
+        for (i = 0; i < platforms2.length; i++) {
+            if (RectCollision(player, platforms2, i)) { 
+                if (player.x_velocity > 0) {//left side
+                    player.x = platforms2[i].x - player.w;
+                    player.x_velocity = 0;
+                } else if (player.x_velocity < 0) {//right side
+                    player.x = platforms2[i].x + platforms2[i].w; 
+                    player.x_velocity = 0;
+                } 
+            }
         }
     }
 }
 
 function verticalCollision() {
-    for (i = 0; i < platforms.length; i++) {
-        if (RectCollision(player, platforms, i)) {
-            if (player.y_velocity > 0) {//top
-                player.y = platforms[i].y - player.h;
-                player.y_velocity = 0;
-                player.jumping = false;
-            } else if (player.y_velocity < 0) {//bottom
-                player.y = platforms[i].y + platforms[i].h;
-                player.y_velocity = 0;
-            } 
+    if (stage == 1) {
+        for (i = 0; i < platforms.length; i++) {
+            if (RectCollision(player, platforms, i)) {
+                if (player.y_velocity > 0) {//top
+                    player.y = platforms[i].y - player.h;
+                    player.y_velocity = 0;
+                    player.jumping = false;
+                } else if (player.y_velocity < 0) {//bottom
+                    player.y = platforms[i].y + platforms[i].h;
+                    player.y_velocity = 0;
+                } 
+            }
+        }
+    } else if (stage == 2) {
+        for (i = 0; i < platforms2.length; i++) {
+            if (RectCollision(player, platforms2, i)) {
+                if (player.y_velocity > 0) {//top
+                    player.y = platforms2[i].y - player.h;
+                    player.y_velocity = 0;
+                    player.jumping = false;
+                } else if (player.y_velocity < 0) {//bottom
+                    player.y = platforms2[i].y + platforms2[i].h;
+                    player.y_velocity = 0;
+                } 
+            }
         }
     }
 }
@@ -190,20 +313,98 @@ function RectCollision(rect1, rect2, i) {
         rect1.y + rect1.h > rect2[i].y)  
 }
 
-function endGame(){
-    if (player.x < 565 + 50 &&
-        player.x + player.w > 565 &&
-        player.y < 120 + 50 &&
-        player.y + player.h > 120) {
-        background = null;
-        //Game is done
-        ctx.fillStyle = 'black';
-        ctx.rect(0, 0, 608, 544);
-        ctx.fill();
+function nextLevel(){
+    if (stage == 1) {
+        if (player.x < 565 + 50 &&
+            player.x + player.w > 565 &&
+            player.y < 120 + 50 &&
+            player.y + player.h > 120 && stage == 1) {
+                player.x = 530;
+                player.y = 0;
+                stage = 2;          
+        }
+    } else if (stage == 2) {
+        if (player.y > cnv.height) {
+            stage = 3;
+        }
+    }
+}
 
-        ctx.font = "30px Comic Sans MS";
-        ctx.fillStyle = "red";
-        ctx.textAlign = "center";
-        ctx.fillText("The End", cnv.width/2, cnv.height/2);
+function drawPlatforms(n, platforms) {
+    ctx.drawImage(platform, platforms[n].x, platforms[n].y, platforms[n].w, platforms[n].h);
+}  
+function drawBats(n) {
+    ctx.drawImage(bat, bats[n].spriteX, bats[n].spriteY, 16, 18, bats[n].x, bats[n].y, bats[n].w, bats[n].h)
+}
+
+function initialise() {
+    if (stage == 1) {
+        //Draw the end place
+        ctx.rect(565, 120, 50, 50);
+
+        //Draw a background
+        ctx.drawImage(background[0], 0, 0, cnv.width, cnv.height);
+
+        //Draw platforms
+        for (let n = 0; n < platforms.length; n++) {
+            drawPlatforms(n, platforms);
+        }
+        //Draw a ground
+        ctx.strokeStyle = 'black';
+        ctx.line(0, 515, 608, 515);
+        ctx.stroke();
+    } else if (stage == 2) {
+        //Draw a background
+        ctx.drawImage(background[1], 0, 0, cnv.width, cnv.height);
+
+        //Draw platforms
+        for (let n = 0; n < platforms2.length; n++) {
+            drawPlatforms(n, platforms2);
+        } 
+        //Draw bats
+        for (let n = 0; n < bats.length; n++) {
+            drawBats(n);
+        }
+    }
+    
+}
+
+setInterval(animateBat, 160);
+function animateBat() {
+    for (let n = 0; n < bats.length; n++) {
+        bats[n].spriteY = 0;
+        bats[n].spriteX += 16;
+        if (bats[n].spriteX >= 64) {
+            bats[n].spriteX = 0;
+        }
+    }
+}
+
+function collisionBats() {
+    if (stage == 2) {
+        for (let n = 0; n < bats.length; n++) {
+            if (RectCollision(player, bats, n)) {
+                player.alive = false;
+            }
+        }
+    }
+}
+
+function batsMove() {
+    if (stage == 2) {
+        for (let n = 1; n < bats.length; n += 2) { 
+             if (bats[n].x >= 608) {
+                bats[n].x -= 9;console.log('bats')
+            } else if (bats[n].x <= 305) {
+                bats[n].x += 9;console.log('bats')
+            } 
+        }
+        for (let n = 0; n < bats.length; n += 2) {
+            if (bats[n].x < -1) {
+                bats[n].x += 9;console.log('bats')
+            } else if (bats[n].x > 305) {
+                bats[n].x -= 9;console.log('bats')
+            }
+        }
     }
 }
